3ab485ac475f941e492341a2d2e8fed7b5adb1cb
Sishaar Rao <sishaar@gmail.com>
2022-07-25 17:06:26 -0700

    fire up selenium

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
-const world = 'world';
+import dotenv from 'dotenv';
 
-export function hello(who: string = world): string {
-  return `Hello ${who}! `;
+import { Browser, Builder, until } from 'selenium-webdriver';
+import { Options, ServiceBuilder } from 'selenium-webdriver/chrome';
+
+dotenv.config({ path: `config/.env.${process.env.NODE_ENV ?? 'development'}` });
+
+const options = new Options();
+
+options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH as string);
+
+// These options are necessary if you'd like to deploy to Heroku
+// options.addArguments("--headless");
+// options.addArguments("--disable-gpu");
+// options.addArguments("--no-sandbox");
+
+async function main () {
+  const serviceBuilder = new ServiceBuilder(process.env.CHROME_DRIVER_PATH);
+  
+  const driver = new Builder()
+    .forBrowser(Browser.CHROME)
+    .setChromeOptions(options)
+    .setChromeService(serviceBuilder)
+    .build();
+
+  try {
+    const res1 = await driver.get('https://www.google.com');
+    const res2 = await driver.wait(until.titleMatches(/Google/));
+    const html = await driver.getPageSource();
+    console.log(`HTML is:\n\n{}\n\n`, html);
+  } finally {
+    await driver.quit();
+  }
 }
+
+main();
