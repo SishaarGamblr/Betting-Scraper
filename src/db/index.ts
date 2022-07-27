81d883933348e087cd0058b96cdf3c358f951d4a
Sishaar Rao <sishaar@gmail.com>
2022-07-27 13:32:02 -0700

    use database url

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,12 +3,5 @@ import Config from 'config';
 
 export default {
   client: 'postgres',
-  connection: {
-    host: Config.get<string>('database.host'),
-    port: Config.get<number>('database.port'),
-    user: Config.get<string>('database.user'),
-    password: Config.get<string>('database.password'),
-    database: Config.get<string>('database.database'),
-    ssl: Config.get<boolean>('database.ssl')
-  }
+  connection: Config.get<string>('database.url')
 };
