c3ec22419d6184ead3d0b428d79ecc2b09123040
Sishaar Rao <sishaar@gmail.com>
2022-12-16 00:30:35 -0500

    add method for fetching NBA lines

--- src/datasources/draftkings.ts
+++ src/datasources/draftkings.ts
@@ -115,6 +115,65 @@ export class DraftKings implements MLB_LineSource, NFL_LineSource {
 
         matchup_index += 2;
       } catch (err) {
+        console.error(err);
+        hasErrored = true;
+      }
+    }
+
+    return matchups;
+  }
+
+  async getNBALines (tableNum: number = 1): Promise<Matchup[]> {
+    const targetURL = 'https://sportsbook.draftkings.com/leagues/basketball/nba';
+    if (await this.driver.getCurrentUrl() !== targetURL) {
+      await this.driver.get(targetURL);
+      await this.driver.wait(until.titleMatches(/Betting Odds, Spreads & Lines/));
+    }
+
+    // Write screenshot
+    // const filename = randomUUID().slice(0, 5) + '.png';
+    // await writeFile(filename, await this.driver.takeScreenshot(), 'base64').then(() => { console.log(`Wrote ${filename}`)});
+    // $ curl -F "file=@myfile.csv" https://file.io
+
+    const matchups: Matchup[] = [];
+  
+    let matchup_index = 1;
+    let hasErrored = false;
+
+    while(!hasErrored) {
+      try {
+        const homeTeam = await this.driver.findElement(By.css(`${this.getTableSelector(tableNum)}${this.getRowSelector(matchup_index)}${this.getTeamNameSelector()}`)).then((element) => element.getText());
+        const homeLine = await this.driver.findElement(By.css(`${this.getTableSelector(tableNum)}${this.getRowSelector(matchup_index)}${this.getLineSelector()}`)).then((element) => element.getText());
+        
+        const awayTeam = await this.driver.findElement(By.css(`${this.getTableSelector(tableNum)}${this.getRowSelector(matchup_index + 1)}${this.getTeamNameSelector()}`)).then((element) => element.getText());
+        const awayLine = await this.driver.findElement(By.css(`${this.getTableSelector(tableNum)}${this.getRowSelector(matchup_index + 1)}${this.getLineSelector()}`)).then((element) => element.getText());
+
+        const dateString = await this.driver.findElement(By.css(`${this.getTableSelector(tableNum)}${this.getNBADateSelector()}`)).then((element) => element.getText());
+
+        const startTimeString = await this.driver.findElement(By.css(`${this.getTableSelector(tableNum)}${this.getRowSelector(matchup_index)}${this.getStartTimeSelector()}`)).then((element) => element.getText());
+
+        console.log(homeTeam, homeLine, awayTeam, awayLine, this.inferMomentFromDateAndTime(dateString, startTimeString));
+
+        matchups.push({
+          home_team: {
+            name: homeTeam
+          },
+          away_team: {
+            name: awayTeam
+          },
+          home_line: {
+            favor: homeLine.charAt(0),
+            odds: parseInt(homeLine.slice(1))
+          },
+          away_line: {
+            favor: awayLine.charAt(0),
+            odds: parseInt(awayLine.slice(1))
+          },
+          date: this.inferMomentFromDateAndTime(dateString, startTimeString)
+        })
+
+        matchup_index += 2;
+      } catch (err) {
         hasErrored = true;
       }
     }
@@ -154,6 +213,9 @@ export class DraftKings implements MLB_LineSource, NFL_LineSource {
     return `table > thead > tr > th.always-left.column-header > div > span > span > span`;
   }
 
+  private getNBADateSelector() {
+    return `table > thead > tr > th.always-left.column-header > div > span > span`
+  }
   private getStartTimeSelector() {
     return `th > a > div > div.event-cell__status > span`;
   }
@@ -161,13 +223,13 @@ export class DraftKings implements MLB_LineSource, NFL_LineSource {
   private getTableSelector(tableNum: number) {
     return `#root > section > section.sportsbook-wrapper__body > section > div.sportsbook-league-page__body > div > div.sportsbook-responsive-card-container__body > div > div > div.sportsbook-card-accordion__children-wrapper > div > div:nth-child(2) > div:nth-child(${tableNum}) > `;
   }
-  
+
   private getRowSelector (row: number) {
     return `table > tbody > tr:nth-child(${row}) > `;
   }
-  
+
   private getTeamNameSelector() {
-    return `th > a > div > div.event-cell__team-info > span > div > div`;
+    return `th > a > div > div.event-cell > div > span > div > div`;
   }
   
   private getLineSelector() {
