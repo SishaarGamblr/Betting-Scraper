4bcdb4dbce915bce8698d1e086c618313a399468
Sishaar Rao <sishaar@gmail.com>
2022-12-17 00:51:30 -0500

    enable router to see health, nba lines controllers

--- src/router.ts
+++ src/router.ts
@@ -2,10 +2,12 @@ import { FastifyInstance } from 'fastify';
 
 import mlbLinesController from './controllers/mlb/lines';
 import nflLinesController from './controllers/nfl/lines';
+import healthController from './controllers/health';
 import nbaLinesController from './controllers/nba/lines';
 
 export default async function router(fastify: FastifyInstance) {
+  fastify.register(healthController);
   fastify.register(mlbLinesController, { prefix: '/mlb' });
   fastify.register(nflLinesController, { prefix: '/nfl' });
-  fastify.register(nbaLinesController, { prefix: '/nba '});
+  fastify.register(nbaLinesController, { prefix: '/nba' });
 }
