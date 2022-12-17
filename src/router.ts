96c4092fc686522430f0594ef9ffcb95c7485183
Sishaar Rao <sishaar@gmail.com>
2022-12-17 00:16:54 -0500

    prettier fixes

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
-import { FastifyInstance } from "fastify";
+import { FastifyInstance } from 'fastify';
 
-import mlbLinesController from "./controllers/mlb/lines";
-import nflLinesController from "./controllers/nfl/lines";
+import mlbLinesController from './controllers/mlb/lines';
+import nflLinesController from './controllers/nfl/lines';
+import nbaLinesController from './controllers/nba/lines';
 
 export default async function router(fastify: FastifyInstance) {
-  fastify.register(mlbLinesController, { prefix: "/mlb" });
-  fastify.register(nflLinesController, { prefix: "/nfl" });
+  fastify.register(mlbLinesController, { prefix: '/mlb' });
+  fastify.register(nflLinesController, { prefix: '/nfl' });
 }
